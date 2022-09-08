cd792b2409c7ac962d30f2aa222ed8759141127d
emasx <29961897+emasx@users.noreply.github.com>
2022-09-08 23:08:28 +0300

    for loop

---
+++ script.js
@@ -0,0 +1,23 @@
+// console.log('Lifting weights repetition 1');
+// console.log('Lifting weights repetition 2');
+// console.log('Lifting weights repetition 3');
+// console.log('Lifting weights repetition 4');
+// console.log('Lifting weights repetition 5');
+// console.log('Lifting weights repetition 6');
+// console.log('Lifting weights repetition 7');
+// console.log('Lifting weights repetition 8');
+// console.log('Lifting weights repetition 9');
+// console.log('Lifting weights repetition 10');
+
+// for loop keeps running while condition is TRUE
+for(let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep}`);
+}
+
+// 1. There are elections in your country! In a small town, there are only 50 voters.
+// Use a for loop to simulate the 50 people voting, by logging a string like this to
+// the console (for numbers 1 to 50): 'Voter number 1 is currently voting'
+
+for (let voter = 1; voter <= 50; voter++) {
+    console.log(`Vote number ${voter} is currently voting`);
+}
